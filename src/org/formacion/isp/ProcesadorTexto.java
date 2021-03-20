c992dc9689e5cbbf5777c02bcdffcb4a4e7613fb
ozaaj <ozaaj@cifpfbmoll.eu>
2021-03-20 20:13:13 +0100

    docs: :construction: Extends of a class and implementation of interface
    I implemented the Interfaces on Correcto.java and ProcesadorTexto.java, and from the class Correcto.java extended from ProcesadorTexto.java

--- src/org/formacion/isp/ProcesadorTexto.java
+++ src/org/formacion/isp/ProcesadorTexto.java
@@ -4,18 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ProcesadorTexto {
-
+public class ProcesadorTexto  implements GeneratorText{
 
 	private List<String> texto = new ArrayList<>();
-	
+
+
+	@Override
 	public void nueva (String palabra) {
 		texto.add(palabra);
 	}
-	
+
+	@Override
 	public String texto () {
 		return texto.stream().collect(Collectors.joining(" "));
 	}
 	
-
 }
