1a83373c3172e7284b4225a142f8d16eeaa35596
ozaaj <ozaaj@cifpfbmoll.eu>
2021-03-20 19:57:46 +0100

    docs: :construction: Corrector.java
    We separated the Corrector method that was in the Class ProcesadorTexto.java, and place it in a new class(Corrector.java)

---
+++ src/org/formacion/isp/Correcto.java
@@ -0,0 +1,11 @@
+public class Correcto {
+    
+    public boolean correcto (Idioma idioma) {
+		for (String palabra: texto) {
+			if (! idioma.diccionario.contains(palabra.toLowerCase())) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
